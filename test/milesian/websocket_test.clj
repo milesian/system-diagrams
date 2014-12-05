ec241e33213be0f48ff17c2dcf51f867db273caa
tangrammer <juanantonioruz@gmail.com>
2014-12-05 13:17:05 +0100

    Ready to use

--- test/milesian/websocket_test.clj
+++ test/milesian/websocket_test.clj
@@ -7,7 +7,7 @@
             [milesian.bigbang :as bigbang]
             [milesian.aop :as aop]
             [milesian.aop.matchers :as aop-matchers]
-            [milesian.aop.utils  :refer (function-invocation)]
+            [milesian.aop.utils  :refer (function-invocation function-return)]
             [milesian.identity :as identity]
             [clojure.string :as st]
             [milesian.system-examples :refer (new-system-map listening talking Listen Talk)]))
@@ -16,17 +16,30 @@
 
 (def system-map (new-system-map))
 
+(def store (atom []))
+
+(defn store-message [m k]
+  (swap! store conj [m k]))
+
+(defn try-to-publish [s]
+  (let [closed (filter (fn[[_ k]] (= k :closed)) @s)]
+    (when (= (count closed) (count (filter (fn[[_ k]] (= k :opened)) @s)))
+      (publish-message (reduce (fn [s [m _]] (str s m "\n")) "" @s))
+      (reset! s []))))
+
+
 (defn logging-function-invocation
   [*fn* this & args]
-  (publish-message (function-invocation *fn* this args))
-  (apply *fn* (conj args this)))
+  (store-message (function-invocation *fn* this args) :opened)
+  (let [res (apply *fn* (conj args this))]
+    (store-message (function-return *fn* this args) :closed)
+    (try-to-publish store)
+    res))
 
 
 (def system (bigbang/expand system-map {:before-start [[identity/add-meta-key system-map]
                                                        [identity/assoc-meta-who-to-deps]]
-                                        :after-start [[aop/wrap (aop-matchers/new-component-matcher :system system-map :components [:a :b :c] :fn logging-function-invocation)]
-
-                                                      ]}))
+                                        :after-start [[aop/wrap (aop-matchers/new-component-matcher :system system-map :components [:a :b :c] :fn logging-function-invocation)]]}))
 
 
 ;;(listening (:b system))
