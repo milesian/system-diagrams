4a76bb8c942aa8b044d8a2e053315df670fbd0cc
juanantonioruz <juanantonioruz@gmail.com>
2014-12-06 01:48:47 +0000

    Fix minor bug

--- test/milesian/websocket_test.clj
+++ test/milesian/websocket_test.clj
@@ -1,19 +1,22 @@
 (ns milesian.websocket-test
   (:require [clojure.repl :refer (apropos dir doc find-doc pst source)]
             [clojure.test :refer :all]
-            [milesian.sequence-diagram :refer (sequence-diagram-invocation server)]
+            [milesian.websocket :refer (server)]
+            [milesian.sequence-diagram :refer (sequence-diagram-invocation)]
             [milesian.bigbang :as bigbang]
             [milesian.aop :as aop]
             [milesian.aop.matchers :as aop-matchers]
             [milesian.aop.utils  :refer (function-invocation function-return)]
             [milesian.identity :as identity]
+
             [milesian.system-examples :refer (new-system-map listening talking Listen Talk)]))
 
 (def system-map (new-system-map))
 
 (def system (bigbang/expand system-map {:before-start [[identity/add-meta-key system-map]
                                                        [identity/assoc-meta-who-to-deps]]
-                                        :after-start [[aop/wrap (aop-matchers/new-component-matcher :system system-map :components [:a :b :c] :fn sequence-diagram-invocation)]]}))
+                                        :after-start [[aop/wrap sequence-diagram-invocation]
+                                                      #_[aop/wrap (aop-matchers/new-component-matcher :system system-map :components [:a :b :c] :fn sequence-diagram-invocation)]]}))
 
 
 
