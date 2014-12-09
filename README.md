cde7e0686acc429010e8d926bcb75cf24d083677
tangrammer <juanantonioruz@gmail.com>
2014-12-09 12:34:20 +0100

    Prepare for 0.1.2 development

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-# milesian/sequence-diagram-dashboard
+# sequence-diagram-dashboard
 
 This is a tool for displaying HTML sequence diagrams.
-
-Using milesian/aop we can log the component call sequence, so it's very easy to render as sequence diagrams.
 
 This project uses [bramp/js-sequence-diagrams](https://github.com/bramp/js-sequence-diagrams) for js/HTML SVG rendering
 
@@ -13,25 +11,12 @@ This project uses [bramp/js-sequence-diagrams](https://github.com/bramp/js-seque
 ## Simple Usage following REPL
 
 ```clojure
-user> (use 'milesian.websocket-test)
-user> (in-ns 'milesian.websocket-test)
-... open your browser (by default port 8088 defined in websocket.clj )
-milesian.websocket-test> (clojure.java.browse/browse-url "http://localhost:8088/")
- ... invoke your components
-milesian.websocket-test> (listening (:b system))
- ... check your browser
- ... invoke your components again
-milesian.websocket-test> (talking (:c system))
- ... check your browser again
 
 ```
 
 
 ## License
 
-The way that server and client communicates with websocket is taken from [samrat/happy-dashboard](https://github.com/samrat/happy-dashboard)
-
-The rest of the code/idea ...
 
 Copyright © 2014 Juan Antonio Ruz (juxt.pro)
 
--- project.clj
+++ project.clj
@@ -1,12 +1,22 @@
-(defproject milesian/sequence-diagram-dashboard "0.1.1-SNAPSHOT"
-  :description "milesian sequence diagram html live renderer"
-  :url "http://example.com/FIXME"
+(defproject tangrammer/sequence-diagram-dashboard "0.1.2-SNAPHSOT"
+  :description "sequence diagram html live renderer"
+  :url "https://github.com/tangrammer/sequence-diagram-dashboard"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[juxt.modular/http-kit "0.5.1"]
-                 [juxt.modular/bidi "0.5.2"]]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha4"]
+                 [com.stuartsierra/component "0.2.2"]
+                 [juxt.modular/http-kit "0.5.1"]
+                 [juxt.modular/bidi "0.5.2"]
+                 [de.ubercode.clostache/clostache "1.4.0"]
+                 [compojure "1.1.8"]
+                 [cheshire                "5.3.1"]
+                 [camel-snake-kebab "0.1.4"]
+                 [org.clojure/tools.logging "0.2.6"]]
+  :main tangrammer.dashboard.sequence-diagram.main
+  :repl-options {:init-ns user}
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]]
+                   :source-paths ["dev/src/clojure" ]
+                   :resource-paths ["test/resources"]}
 
-  :profiles {:dev {:dependencies [[milesian/bigbang "0.1.1-SNAPSHOT"]
-                                  [milesian/aop "0.1.2-SNAPSHOT"]
-                                  [milesian/system-examples "0.1.1-SNAPSHOT"]
-                                  [milesian/identity "0.1.2-SNAPSHOT"]]}})
+             :uberjar {:main tangrammer.dashboard.sequence-diagram.main
+                       :aot [tangrammer.dashboard.sequence-diagram.main]}})
